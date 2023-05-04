c91104095cbf6cb98cf239fa91148959886ba45e
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-05-03 21:26:48 -0300

    Adding a life logic for hangman

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
+user_life = 6
+
 selected_word = random.choice(word_list)
 
 word_blank_spaces = len(selected_word)
@@ -11,13 +13,20 @@ array_of_answer = []
 for space in range(0, word_blank_spaces):
   array_of_answer.append("_")
 
-print(array_of_answer)
+while user_life > 0 and "_" in array_of_answer:
+  print(array_of_answer)
+  user_choice = input("Guess a letter: ")
 
-user_choice = input("Guess a letter: ")
+  for index, letter in enumerate(selected_word):
+    if user_choice.lower() == letter.lower():
+      array_of_answer[index] = letter
 
-for index, letter in enumerate(selected_word):
-  if user_choice.lower() == letter.lower():
-    array_of_answer[index] = letter
+  if user_choice.lower() not in selected_word.lower():
+    user_life -= 1
+    print(f"You have {user_life} lives left")
+
+
+
   
 
-print(array_of_answer)
+

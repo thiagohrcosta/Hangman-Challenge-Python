c9fccf3a401a11b1e7f5e49756d9d4f939a22e54
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-05-05 22:40:08 -0300

    Adding loops and If else statements to create the game flow

--- app/main.py
+++ app/main.py
@@ -1,5 +1,62 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 
 user_life = 6
@@ -10,10 +67,16 @@ word_blank_spaces = len(selected_word)
 
 array_of_answer = []
 
+wrong_attempts = 0
+
 for space in range(0, word_blank_spaces):
   array_of_answer.append("_")
 
 while user_life > 0 and "_" in array_of_answer:
+  print("-------------------")
+  if user_life > 0:
+    print(stages[int(user_life)])  
+  
   print(array_of_answer)
   user_choice = input("Guess a letter: ")
 
@@ -24,4 +87,13 @@ while user_life > 0 and "_" in array_of_answer:
   if user_choice.lower() not in selected_word.lower():
     user_life -= 1
     print(f"You have {user_life} lives left")
-    
+
+if user_life == 0 and "_" in array_of_answer:
+  print(stages[0])
+  print("You lose")
+elif "_" not in array_of_answer:
+  print("You win")
+
+  
+
+
